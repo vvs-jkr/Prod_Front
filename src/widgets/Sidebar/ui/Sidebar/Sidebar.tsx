648ea71ea5ad23d53829052fc96c223cdc3fbbca
Vitaliy S <vvs.jkr@gmail.com>
2025-06-17 20:01:02 +0300

    fix

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,38 +1,39 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
-import { Button } from 'shared/ui/Button/Button';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
-import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
-import { useTranslation } from 'react-i18next';
-import cls from './Sidebar.module.scss';
+import React from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { useState } from 'react'
+import { Button } from 'shared/ui/Button/Button'
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher'
+import { useTranslation } from 'react-i18next'
+import cls from './Sidebar.module.scss'
 
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false)
 
-    const onToggle = () => {
-        setCollapsed((prev) => !prev);
-    };
+  const onToggle = () => {
+    setCollapsed((prev) => !prev)
+  }
 
-    const { t } = useTranslation();
+  const { t } = useTranslation()
 
-    return (
-        <div
-            data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
-                className,
-            ])}
-        >
-            <Button data-testid="sidebar-toggle" onClick={onToggle}>
-                {t('Свернуть')}
-            </Button>
-            <div className={cls.switchers}>
-                <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div
+      data-testid="sidebar"
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        {t('Свернуть')}
+      </Button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
+      </div>
+    </div>
+  )
+}
